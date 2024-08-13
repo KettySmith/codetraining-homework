4565f9fe71c81a5c8a7bbcedad26227f65fc2610
KettySmith <tqlyixin111@163.com>
2024-08-13 17:21:32 +0800

    frontend:前端初始化

---
+++ user-frontend/src/views/user/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="user-container">
+    <div class="user-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'User',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
